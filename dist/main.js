4f15d5a8fd041aaef36ed750c316ba75b53b1e51
Barzi Jaf <barzi4greatness@gmail.com>
2021-03-28 14:01:10 +0200

    added babel

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,2 @@
-console.log("testin");
+console.log("Hello World"),console.log("barzi"),console.log({name:"barzi",age:"19"});
+//# sourceMappingURL=main.js.map
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const test = "testin";
+const test = "Hello World";
+const myName = "barzi";
+
+const myObject = {
+  name: "barzi", 
+  age: "19"
+};
 console.log(test);
+console.log(myName);
+console.log(myObject);
